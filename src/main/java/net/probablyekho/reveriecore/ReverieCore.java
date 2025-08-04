d4b43fbcd2961012ade446bfede7cc30456b4971
ProbablyEkho <probablyekho@yahoo.com>
2025-08-05 01:19:24 +0200

    cobbled andesite, diorite and granite blocks and slabs removed battery blocks

--- src/main/java/net/probablyekho/reveriecore/ReverieCore.java
+++ src/main/java/net/probablyekho/reveriecore/ReverieCore.java
@@ -1,24 +1,12 @@
 package net.probablyekho.reveriecore;
 
 import net.probablyekho.reveriecore.block.ModBlocks;
-import net.probablyekho.reveriecore.item.ModFoodComponents;
 import net.probablyekho.reveriecore.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -28,10 +16,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ReverieCore.MODID)
@@ -66,15 +50,26 @@ public class ReverieCore {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
-            event.accept(ModItems.BUTTER);
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BATTERY);
         }
         if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
             event.accept(ModItems.BUTTER);
             event.accept(ModItems.FISHSTICKS);
         }
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModBlocks.BATTERY_BLOCK);
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.COBBLED_ANDESITE);
+            event.accept(ModBlocks.COBBLED_DIORITE);
+            event.accept(ModBlocks.COBBLED_GRANITE);
+            event.accept(ModBlocks.COBBLED_ANDESITE_SLAB);
+            event.accept(ModBlocks.COBBLED_DIORITE_SLAB);
+            event.accept(ModBlocks.COBBLED_GRANITE_SLAB);
+            event.accept(ModBlocks.COBBLED_ANDESITE_WALL);
+            event.accept(ModBlocks.COBBLED_DIORITE_WALL);
+            event.accept(ModBlocks.COBBLED_GRANITE_WALL);
+            event.accept(ModBlocks.COBBLED_ANDESITE_STAIRS);
+            event.accept(ModBlocks.COBBLED_DIORITE_STAIRS);
+            event.accept(ModBlocks.COBBLED_GRANITE_STAIRS);
         }
     }
 
--- src/main/java/net/probablyekho/reveriecore/block/ModBlocks.java
+++ src/main/java/net/probablyekho/reveriecore/block/ModBlocks.java
@@ -2,9 +2,9 @@ package net.probablyekho.reveriecore.block;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -17,9 +17,45 @@ public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(ReverieCore.MODID);
 
-    public static final DeferredBlock<Block> BATTERY_BLOCK = registerBlock("battery_block",
+    public static final DeferredBlock<Block> COBBLED_ANDESITE = registerBlock("cobbled_andesite",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .requiresCorrectToolForDrops().strength(3f, 6f).sound(SoundType.METAL)));
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_DIORITE = registerBlock("cobbled_diorite",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_GRANITE = registerBlock("cobbled_granite",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+
+    public static final DeferredBlock<Block> COBBLED_ANDESITE_SLAB = registerBlock("cobbled_andesite_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_DIORITE_SLAB = registerBlock("cobbled_diorite_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_GRANITE_SLAB = registerBlock("cobbled_granite_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+
+    public static final DeferredBlock<Block> COBBLED_ANDESITE_WALL = registerBlock("cobbled_andesite_wall",
+            () -> new WallBlock(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_DIORITE_WALL = registerBlock("cobbled_diorite_wall",
+            () -> new WallBlock(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_GRANITE_WALL = registerBlock("cobbled_granite_wall",
+            () -> new WallBlock(BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+
+    public static final DeferredBlock<Block> COBBLED_ANDESITE_STAIRS = registerBlock("cobbled_andesite_stairs",
+            () -> new StairBlock(ModBlocks.COBBLED_ANDESITE.get().defaultBlockState(),BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_DIORITE_STAIRS = registerBlock("cobbled_diorite_stairs",
+            () -> new StairBlock(ModBlocks.COBBLED_DIORITE.get().defaultBlockState(),BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> COBBLED_GRANITE_STAIRS = registerBlock("cobbled_granite_stairs",
+            () -> new StairBlock(ModBlocks.COBBLED_GRANITE.get().defaultBlockState(),BlockBehaviour.Properties.of()
+                    .instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(2f, 6f).sound(SoundType.STONE)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
