6870d52e7f1dd443efa3bd7b378ebc419522fcf2
ProbablyEkho <probablyekho@yahoo.com>
2025-09-27 16:05:04 +0200

    underwater shulkers and other tweaks

---
+++ src/main/java/net/probablyekho/reveriecore/mixin/ShulkerEntityMixin.java
@@ -0,0 +1,19 @@
+package net.probablyekho.reveriecore.mixin;
+
+import net.minecraft.world.entity.monster.Shulker;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Shulker.class)
+public abstract class ShulkerEntityMixin {
+    @Inject(
+        method = "teleportSomewhere",
+        at = @At(value = "HEAD"),
+        cancellable = true
+    )
+    private void noShulkerTeleport(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+}
